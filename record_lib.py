7d563461caa99fac2c7054eafd6788d916c314e9
Rong-Tao <trsjtu17@gmail.com>
2023-11-09 11:04:03 +0800

    update test mode

--- record_lib.py
+++ record_lib.py
@@ -99,3 +99,63 @@ def image_save(client, base_folder = 'img'):
         plt.axis('off')
         plt.savefig(png_filename, bbox_inches='tight', pad_inches=0)
         plt.close()
+
+
+def image_save_test(client, base_folder = 'img'):
+    # Pause the simulation
+    
+    # Request various types of images
+    responses = client.simGetImages([
+        airsim.ImageRequest("front_left", airsim.ImageType.Scene, False, False),
+        airsim.ImageRequest("front_right", airsim.ImageType.Scene, False, False),
+        ])
+
+    # Specify the base folder to save images
+    
+    folders = ['left_raw', 'right_raw']
+
+    # Ensure folders exist
+    for folder in folders:
+        path = os.path.join(base_folder, folder)
+        if not os.path.exists(path):
+            os.makedirs(path)
+            
+    timestamp = str(responses[0].time_stamp)
+    # Process and save each response
+    for i, response in enumerate(responses):
+        
+        camera = 'left' if i < 1 else 'right'
+
+        if response.image_type == airsim.ImageType.Scene:
+            folder = f"{camera}_raw"
+
+        filename = os.path.join(base_folder, folder, f"{timestamp}")
+        png_filename = f"{filename}.png"
+
+        # Case 1: Pixels as floating-point values
+        if response.pixels_as_float:
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_float)))
+            img = np.array(response.image_data_float).reshape(response.height, response.width, -1)
+
+        # Case 2: Pixels as uint8 values
+        else:
+            print("Type %d, size %d" % (response.image_type, len(response.image_data_uint8)))
+            img = np.frombuffer(response.image_data_uint8, dtype=np.uint8).reshape(response.height, response.width, -1)
+
+        
+        img = np.flipud(img)
+        img = np.fliplr(img)
+
+        # Save the image using Matplotlib without showing it
+        default_dpi = plt.rcParams['figure.dpi']
+        plt.figure(figsize=(img.shape[1] / default_dpi, img.shape[0] / default_dpi),
+                   dpi=default_dpi)
+        if img.shape[2] == 1:
+            plt.imshow(img, cmap='gray', aspect='auto')
+        else:
+            img = img[:, :, ::-1]
+            plt.imshow(img, aspect='auto')
+
+        plt.axis('off')
+        plt.savefig(png_filename, bbox_inches='tight', pad_inches=0)
+        plt.close()
